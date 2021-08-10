1cf0624dc6348909804b632d069df488b371ce76
Digvijay <digvijayyamagekar@gmail.com>
2021-08-10 15:33:24 +0530

    Added route for login page's ajax call

--- routers/api.js
+++ routers/api.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const CheckUserInDB = require('../controllers/userAndEmailChecker')
+const loginCreds = require('../controllers/loginCredChecker')
+app.use(express.urlencoded({ extended: false }));
 
-router
-    .get('/userCheck', async (req, res) => {
-        let result = await CheckUserInDB.checker(req.query)
-        res.json(result);
-    })
+router.get('/userCheck', async (req, res) => {
+    let result = await CheckUserInDB.checker(req.query)
+    res.json(result);
+})
+
+router.post('/loginCheck', async (req, res) => {
+    let result = await loginCreds.checker(req.body)
+
+    if (Number(result)) {
+        res.send('1')
+    } else {
+        res.send(result)
+    }
+
+})
 
 module.exports = router
 
