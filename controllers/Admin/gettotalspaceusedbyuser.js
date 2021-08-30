d6d9abf6a9fcbaaf0d61d9520a8672d2ff29778e
digvijay <digvijayyamagekar@gmail.com>
2021-08-30 09:38:16 +0530

    code optimization

--- controllers/Admin/gettotalspaceusedbyuser.js
+++ controllers/Admin/gettotalspaceusedbyuser.js
@@ -3,7 +3,7 @@ const DB = require('../../db-config')
 /**
  *returns the total storage space (in mb) used by users 
  * @param {number} user_id 
- * @return {number} 
+ * @return {number} total space used by the user of user_id
  */
 async function totalusedspace(user_id) {
     let totalusedspace = `SELECT SUM(filesize)
@@ -11,15 +11,16 @@ async function totalusedspace(user_id) {
     WHERE user_id = '${user_id}';`
 
     let sum = await DB.executeQuery(totalusedspace)
+
     let totalsum = 0;
     if (sum[0].sum == null) {
         totalsum = 0
-        console.log(totalsum);
         return totalsum
     } 
     return sum[0].sum;
 }
 
+totalusedspace(40);
 
 module.exports = {
     totalusedspace
