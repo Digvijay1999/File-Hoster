1fed6930ed79da6a99e7802ec1de8004a3b49d68
digvijay <digvijayyamagekar@gmail.com>
2021-09-20 18:41:30 +0530

    minor update

--- routers/homepage.js
+++ routers/homepage.js
@@ -43,8 +43,9 @@ router
         }
         //login here, create session , sent to next page
         let loginquery = `SELECT username, userpassword,access FROM user_credentials WHERE username = '${req.body.username}' `
+        var login_credential;
         try {
-            var login_credential = await DB.executeQuery(loginquery)
+            login_credential = await DB.executeQuery(loginquery)
         } catch (error) {
             console.log("error while retrieving info from user_credentials table(database error)");
         }
