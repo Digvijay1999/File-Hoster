63c4ae7d3143dfcf2463ba9ae7fa317097f1dddb
Digvijay <digvijayyamagekar@gmail.com>
2022-02-05 23:24:13 +0530

    db update

--- db-config.js
+++ db-config.js
@@ -4,13 +4,21 @@ const _ = require('lodash');
 const Fs = require('graceful-fs');
 const { off } = require('process');
 
+// let dbConfig = {
+//     user: process.env.HEROKU_USER,
+//     host: process.env.HEROKU_HOST,
+//     database: process.env.HEROKU_DB,
+//     password: process.env.HEROKU_PASSWORD,
+//     port: process.env.PORT || "5432",
+//     ssl: { rejectUnauthorized: false },
+// }
 let dbConfig = {
-    user: process.env.HEROKU_USER,
-    host: process.env.HEROKU_HOST,
-    database: process.env.HEROKU_DB,
-    password: process.env.HEROKU_PASSWORD,
-    port: process.env.PORT || "5432",
-    ssl: { rejectUnauthorized: false },
+    user: "sjayqkttpvnrfr",
+    host: "ec2-54-158-247-97.compute-1.amazonaws.com",
+    database: "d4hdvoc940aju",
+    password: "c9ce6a3a387931e105155b09931f30151d4a10913aac2e096ce2781ff3546c03",
+    port: "5432",
+    ssl: { rejectUnauthorized: false }
 }
 
 /**
