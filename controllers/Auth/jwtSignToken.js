1997b457a772b76e746be7a0bc7d4c06a6c10552
digvijay yamagekar persistent <digvijay_yamagekar@persistent.com>
2022-11-23 20:01:07 +0530

    changes secreat key to string for testing

--- controllers/Auth/jwtSignToken.js
+++ controllers/Auth/jwtSignToken.js
@@ -2,6 +2,6 @@ var jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 module.exports = function signToken(payLoad) {
-    var token = jwt.sign(payLoad, JSONSecreat, { algorithm: 'RS256', issuer: "File-Hoster", expiresIn: "2 days" });
+    var token = jwt.sign(payLoad, "process.env.JSONsecret", { algorithm: 'RS256', issuer: "File-Hoster", expiresIn: "2 days" });
     return token;
 }
--- controllers/Auth/jwtVerifyToken.js
+++ controllers/Auth/jwtVerifyToken.js
@@ -1,10 +1,10 @@
-const jwt = require('jasonwebtoken')
+const jwt = require('jsonwebtoken')
 
 require('dotenv').config()
 
 module.exports = function verifyToken(req, res, next) {
     try {
-        let payload = jwt.verify(req.cookies.token, JSONSecreat, { algorithms: ['RS256'] })
+        let payload = jwt.verify(req.cookies.token, "process.env.JSONSecreat", { algorithms: ['RS256'] })
         payload = JSON.parse(payload)
         req.cookies.user = payload.username
         req.cookies.userID = payload.userID
