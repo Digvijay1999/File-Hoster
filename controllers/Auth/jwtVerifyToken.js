7448acc8886a8069f20d416fcf94cd64ac97bc46
Digvijay <digvijayyamagekar@gmail.com>
2022-11-23 23:52:03 +0530

    set default algo for signing the token

--- controllers/Auth/jwtVerifyToken.js
+++ controllers/Auth/jwtVerifyToken.js
@@ -4,12 +4,11 @@ require('dotenv').config()
 
 module.exports = function verifyToken(req, res, next) {
     try {
-        let payload = jwt.verify(req.cookies.token, "process.env.JSONSecreat", { algorithms: ['RS256'] })
-        payload = JSON.parse(payload)
+        let payload = jwt.verify(req.cookies.token, "process.env.JSONSecreat")
         req.cookies.user = payload.username
         req.cookies.userID = payload.userID
         next();
-    } catch {
-        res.end("please login first")
+    } catch (error) {
+        res.end("please login first " + error)
     }
 }
