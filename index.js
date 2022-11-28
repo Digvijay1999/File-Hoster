075d797207b7dcb758510aed9a56ee15ab295f25
Digvijay <digvijayyamagekar@gmail.com>
2022-11-28 07:57:44 +0530

    added cors and static folder for files dir

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
 const fs = require('fs');
 const homepage = require('./routers/homepage')
 const api = require('./routers/api')
@@ -8,16 +7,18 @@ const fileHandler = require('./routers/fileHandler')
 const admin = require('./routers/adminHandler')
 const layouts = require('express-ejs-layouts')
 const cookieparser = require('cookie-parser')
-const upload = require('express-fileupload')
 const DB = require('./db-config');
 const path = require('path');
 
-app.use(upload());
+const cors = require('cors');
+app.use(cors({ origin: ["http://localhost:8000/","http://127.0.0.1:8000/"], methods: "POST" }))
+
 app.use(cookieparser());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
+//app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/files', express.static(__dirname + 'public/files'))
 
 app.set('view engine', 'ejs')
 app.use(layouts);
