c8e937cd19bf20bb41962ec89da218698a94c12b
Digvijay <digvijayyamagekar@gmail.com>
2022-11-28 07:58:09 +0530

    generates s3 signed link

---
+++ controllers/fileHandlers/s3urlGenerator.js
@@ -0,0 +1,23 @@
+const aws = require("aws-sdk")
+require("dotenv").config()
+
+const s3 = new aws.S3({
+    region: "ap-south-1",
+    accessKeyId: process.env.Access_key_ID,
+    Secret_access_key: process.env.Secret_access_key,
+    signatureVersion: 'v4'
+})
+
+async function generateS3URL(filename) {
+    const params = ({
+        Bucket: "storagebucketforfilehosterapp",
+        Key: filename,
+        Expires: 30
+    })
+    const uploadURL = await s3.getSignedUrlPromise('putObject', params)
+    return uploadURL
+}
+
+module.exports = {
+    generateS3URL
+}
