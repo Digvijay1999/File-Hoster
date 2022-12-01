63ff7fb58084593edd748f0b3dfad984a4965eda
Digvijay <digvijayyamagekar@gmail.com>
2022-12-01 09:20:25 +0530

    get total files uploaded by user

---
+++ controllers/fileHandlers/getFileCountOfUser.js
@@ -0,0 +1,9 @@
+const DB = require("../../db-config")
+
+async function getFileCount(userid) {
+    let qry = `SELECT COUNT(user_id) as total FROM user_files WHERE user_id = ${userid}`
+    let res = await DB.executeQuery(qry);
+    return res.total
+}
+
+module.exports = { getFileCount }
