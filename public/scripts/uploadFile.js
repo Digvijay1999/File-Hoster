10c331f67d98f4c330f387635e67248ef22d9117
Digvijay <digvijayyamagekar@gmail.com>
2022-12-01 09:21:33 +0530

    xml request to server to make file entry

--- public/scripts/uploadFile.js
+++ public/scripts/uploadFile.js
@@ -21,22 +21,28 @@ file.onchange = async (e) => {
     xrh.onload = () => {
         let res = xrh.responseText
         console.log(res)
-        uploadTos3(res, new FormData(form), updatePercentage, filename)
+        uploadTos3(res, new FormData(form), updatePercentage, filename, size)
     }
 }
 
-function updatePercentage(percentage, filename) {
+function updatePercentage(percentage, filename, size) {
     if (percentage == 100) {
         document.getElementById("content").style.display = "none"
         document.getElementById("uploadingprocess").textContent = percentage
 
         let node = document.createElement('span').textContent = `${filename}   DONE`
         document.getElementById('append').innerHTML = node
+
+        var xrh = new XMLHttpRequest()
+        xrh.open("POST", "http://localhost:8000/api/fileuploaded")
+        xrh.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+        xrh.send(`filename=${filename}&size=${size}`)
+
     }
     document.getElementById("progressbar").style.width = `${percentage}%`
 }
 
-function uploadTos3(url, file, updatePercentage, filename) {
+function uploadTos3(url, file, updatePercentage, filename, size) {
     return new Promise(function (resolve, reject) {
 
         const xhr = new XMLHttpRequest();
@@ -57,7 +63,7 @@ function uploadTos3(url, file, updatePercentage, filename) {
             xhr.upload.onprogress = (e) => {
                 if (e.lengthComputable) {
                     var percentComplete = Math.ceil((e.loaded / e.total) * 100);
-                    updatePercentage(percentComplete, filename);
+                    updatePercentage(percentComplete, filename, size);
                 }
             };
         }
