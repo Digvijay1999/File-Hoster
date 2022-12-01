607a1c8c5245fd985e515f24926e2fcfc25cbd0c
Digvijay <digvijayyamagekar@gmail.com>
2022-12-02 00:37:25 +0530

    fix return value

--- controllers/fileHandlers/getFileCountOfUser.js
+++ controllers/fileHandlers/getFileCountOfUser.js
@@ -3,7 +3,7 @@ const DB = require("../../db-config")
 async function getFileCount(userid) {
     let qry = `SELECT COUNT(user_id) as total FROM user_files WHERE user_id = ${userid}`
     let res = await DB.executeQuery(qry);
-    return res.total
+    return res[0].total
 }
 
 module.exports = { getFileCount }
